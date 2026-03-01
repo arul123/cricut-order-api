fc64a0f99d4603ef576c2e443764e57309536b31
arul123 <arul_anandam@yahoo.com>
2026-02-28 21:08:14 -0700

    feat: complete Cricut Orders challenge

---
+++ Cricut.Orders.Api/Controllers/OrdersController.cs
@@ -0,0 +1,34 @@
+﻿using Cricut.Orders.Api.Mappings;
+using Cricut.Orders.Api.ViewModels;
+using Cricut.Orders.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cricut.Orders.Api.Controllers
+{
+    [Route("v1/orders")]
+    [ApiController]
+    public class OrdersController : Controller
+    {
+        private readonly IOrderDomain _orderDomain;
+
+        public OrdersController(IOrderDomain orderDomain)
+        {
+            _orderDomain = orderDomain;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<OrderViewModel>> CreateNewOrderAsync([FromBody] NewOrderViewModel newOrderVM)
+        {
+            var newOrder = newOrderVM.ToDomainModel();
+            var savedOrder = await _orderDomain.CreateNewOrderAsync(newOrder);
+            return Ok(savedOrder.ToViewModel());
+        }
+
+        [HttpGet("customer/{customerId}")]
+        public async Task<ActionResult<OrderViewModel[]>> GetOrdersForCustomerAsync(int customerId)
+        {
+            var orders = await _orderDomain.GetOrdersForCustomerAsync(customerId);
+            return Ok(orders.Select(o => o.ToViewModel()).ToArray());
+        }
+    }
+}
---
+++ Cricut.Orders.Domain/OrderDomain.cs
@@ -0,0 +1,31 @@
+﻿using Cricut.Orders.Domain.Models;
+
+namespace Cricut.Orders.Domain
+{
+    public interface IOrderDomain
+    {
+        Task<Order> CreateNewOrderAsync(Order order);
+        Task<Order[]> GetOrdersForCustomerAsync(int customerId);
+    }
+
+    public class OrderDomain : IOrderDomain
+    {
+        private readonly IOrderStore _orderStore;
+
+        public OrderDomain(IOrderStore orderStore)
+        {
+            _orderStore = orderStore;
+        }
+
+        public async Task<Order> CreateNewOrderAsync(Order order)
+        {
+            var updatedOrder = await _orderStore.SaveOrderAsync(order);
+            return updatedOrder;
+        }
+
+        public async Task<Order[]> GetOrdersForCustomerAsync(int customerId)
+        {
+            return await _orderStore.GetAllOrdersForCustomerAsync(customerId);
+        }
+    }
+}
